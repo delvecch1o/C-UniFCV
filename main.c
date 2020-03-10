2bf5d7c26ff82c75352e5cfede7bf3837ebcc632
delvecch1o <61480650+delvecch1o@users.noreply.github.com>
2020-03-10 19:34:46 -0300

    funcoes sem parametros

--- main.c
+++ main.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// protótipo da função
-
-int quadrado (int a);
-
-int main () {
-	int n1, n2;
-	printf("Digite um numero: ");
-	scanf ("%d", &n1);
-	n2 = quadrado (n1);
-	printf("O quadrado de %d eh %d\n",n1,n2);
-	system ("pause");
-	return 0;
+int imprime () { // se colocar o [int imprime (void)] da erro
+	printf ("Teste de funcoes \n");
 }
-int quadrado (int a) {
-	return a * a;
+int main () {
+	  imprime ();
+	  imprime (5);
+	  imprime (5, 'a');
+	  return 0;	
+	
 }
