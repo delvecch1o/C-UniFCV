6b668d1eca61b3164914e7e09e2da86cc80b36a5
delvecch1o <61480650+delvecch1o@users.noreply.github.com>
2020-03-11 19:39:55 -0300

    funcoes ex 04

--- main.c
+++ main.c
@@ -5,13 +5,15 @@ struct ponto {
 	int x,y;
 };
 
-void imprime (struct ponto p){
-	printf("x = %d\n",p.x);
-		printf("x = %d\n",p.y);
-}
+void atribui (struct ponto *p){
+	(*p).x = 10;
+	(*p).y = 20; }
+
 int main (){
-	struct ponto p1 = {10,20};
-	imprime (p1);
+	struct ponto p1;
+	atribui (&p1);
+	printf("x = %d\n",p1.x);
+	printf("x = %d\n",p1.y);
 	system ("pause");
 	return 0;
 }
