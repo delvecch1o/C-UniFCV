828df13be8c40484bf063fd5b5dd9d8fa07e9b8e
delvecch1o <61480650+delvecch1o@users.noreply.github.com>
2020-03-17 20:47:34 -0300

    exercicio 01 fatorial

---
+++ exercicio 01 recursividade .c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fatorial (int n) {
+	if (n == 0)
+		return 1;
+	else 
+		return n * fatorial (n - 1); // metodo geral ele vai chamar a função recussivamente.
+}
+
+int main () {
+	int numero;
+	printf ("Digite um numero fatorial:\n");
+	scanf("%d", &numero);
+	printf ("O numero fatorial %d eh %d ",numero,fatorial (numero));
+	}
